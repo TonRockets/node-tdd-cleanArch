801c2ec29a479c04f0e54b8e7ce2544d56af6f8c
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-04-04 09:05:45 -0300

    refactor: create helper to clean the controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,14 +3,15 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
+    const requiredFields = ['name', 'email', 'body']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    return {
+      statusCode: 401,
+      body: 'Unauthorized'
     }
-
-    return badRequest(new MissingParamError('body'))
   }
 }
