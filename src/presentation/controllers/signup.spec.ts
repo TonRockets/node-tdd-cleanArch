7d76999d0fc31324ae10d303a13db7ed629a2d4c
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-04-04 08:28:35 -0300

    refactor: create customize error

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from './signup'
+import { MissingParamError } from '../errors/missing-param-error'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
@@ -17,7 +18,7 @@ describe('SignUp Controller', () => {
     because toBe compare type and value of an object, the toEqual just
     compare a value.
     */
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
 
   test('Should return 400 if no email is provided', () => {
@@ -36,6 +37,26 @@ describe('SignUp Controller', () => {
     because toBe compare type and value of an object, the toEqual just
     compare a value.
     */
-    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
+  })
+
+  test('Should return 400 if no body is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'nay_name',
+        email: 'any_email@email.com',
+        password: 'any_pass',
+        passwordConfirmation: 'any_pass'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+
+    /* When we compare object we need to use 'toEqual' instead toBe,
+    because toBe compare type and value of an object, the toEqual just
+    compare a value.
+    */
+    expect(httpResponse.body).toEqual(new MissingParamError('body'))
   })
 })
