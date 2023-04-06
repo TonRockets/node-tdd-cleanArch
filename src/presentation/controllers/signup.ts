78c9cf4976c3ae04c9faff450327415c2c3f9b07
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-04-06 17:02:44 -0300

    test: ensure signupController calls emailValidator with correct email

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -25,7 +25,7 @@ export class SignUpController implements Controller {
         return badRequest(new MissingParamError(field))
       }
     }
-    const isValidEmail = this.emailValidator.isValid(httpRequest.body)
+    const isValidEmail = this.emailValidator.isValid(httpRequest.body.email)
     if (!isValidEmail) {
       return badRequest(new InvalidParamError('email'))
     }
