9820186f7e9e32b990a85c7cf2a1d4aaa58b7516
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-06-27 16:41:54 -0300

    feat: create middlewares and routes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import setUpMidd from './middlewares'
+import setUpRoutes from './routes'
 
 const app = express()
 setUpMidd(app)
-
+setUpRoutes(app)
 export default app
---
+++ src/main/config/routes.ts
@@ -0,0 +1,9 @@
+import { type Express, Router } from 'express'
+import fastG from 'fast-glob'
+export default (app: Express): void => {
+  const router = Router()
+  app.use('/api', router)
+  fastG.sync('**/src/main/routes/**routes.ts').map(async (item) => {
+    (await import(`../../../${item}`)).default(router)
+  })
+}
