12f794b664e8e5d200bf04b541be5492cde3e436
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-07-11 13:32:45 -0300

    feat: finish logRepositoryDecorator

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,12 +1,19 @@
+import { type LogErrorRepository } from '../../data/protocols/log-error-repository'
 import type { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 
 export class LogControllerDecorator implements Controller {
   private readonly controller: Controller
-  constructor(controller: Controller) {
+  private readonly logErrorRepository: LogErrorRepository
+  constructor(controller: Controller, logErrorRepository: LogErrorRepository) {
     this.controller = controller
+    this.logErrorRepository = logErrorRepository
   }
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    return await this.controller.handle(httpRequest)
+    const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.log(httpResponse.body.stack)
+    }
+    return httpResponse
   }
 }
