fa59f333f809e8dc6337c2cb295f573ddd9009ba
TonRockets <wellington.pereira@al.forsoft.org.br>
2023-07-13 17:38:34 -0300

    feat: create log-error-repository inteface

--- src/data/protocols/log-error-repository.ts
+++ src/data/protocols/log-error-repository.ts
@@ -1,3 +1,5 @@
 export interface LogErrorRepository {
-  log(stack: string): Promise<void>
+  // A classe concreta que ira implementar a interface precia que
+  // o metodo da interface tambem tenha o seu nome concreto e nao abstrato/generico
+  logError(stack: string): Promise<void>
 }
